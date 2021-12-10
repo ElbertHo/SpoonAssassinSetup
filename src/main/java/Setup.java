15264bf100cb1c863637f0ee45f23e0ce060675b
ElbertHo <eho2025@pingry.org>
2021-12-10 12:08:29 -0500

    Set up the file structure, add a template for the names file

---
+++ src/main/java/Setup.java
@@ -0,0 +1,13 @@
+/**
+ * Setup a spoon assasins game
+ * Reads in the necessary names from the names file in the docs folder
+ * @author Elbert Ho
+ * @version 1.0
+ */
+public class Setup
+{
+  public static void main(String[] args)
+  {
+
+  }
+}
